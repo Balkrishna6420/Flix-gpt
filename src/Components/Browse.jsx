bf9a26bed2514120d41e12b94636e570a6f2d6cf
Balkrishna Jha <bkrishnajha494@gmail.com>
2026-08-11 00:09:05 +0530

    Secondary Container with Customs hooks of Videos Section

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,16 +1,21 @@
-
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRated from "../hooks/useTopRated";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 import SecondaryConainer from "./SecondaryConainer";
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRated();
+  useUpcomingMovies();
   return (
     <div>
       <Header />
       <MainContainer />
-      <SecondaryConainer/>
+      <SecondaryConainer />
     </div>
   );
 };
